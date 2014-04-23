84125f3a370196277aced0ccda8c90b7563ba7cc
Valerio.Gheri <valerio.gheri@gmail.com>
2014-04-23 12:45:18 +0200

    Test commit

--- app_server/index.js
+++ app_server/index.js
@@ -5,17 +5,17 @@
  */
 'use strict';
 
-var winston = require('winston');
+//var winston = require('winston');
 var webserver = require('./server');
 // We will log normal api operations into api.log
 console.info("starting logger...");
-winston.add(winston.transports.File, {
+/*winston.add(winston.transports.File, {
 	filename: "app_server/logs/api.log"
 });
 // We will log all uncaught exceptions into exceptions.log
 winston.handleExceptions(new winston.transports.File({
 	filename: "app_server/logs/exceptions.log"
-}));
+}));*/
 console.info("Starting Web and Primus server ...");
 process.on('uncaughtException', function (err) {
     console.log( "UNCAUGHT EXCEPTION " );
